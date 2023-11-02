bc7156a9b75b1c07790eafb187fa0ce007b7670b
MarcosRubi <danielhernandez9980@gmail.com>
2023-11-02 14:26:54 -0600

    Boton de ver todos los tours cambia el estado del menu

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/jsx-indent */
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useMenuStore } from '../../store/menuStore'
 
 function Menu () {
   const [menuVisible, setMenuVisible] = useState(false)
   const [subMenuVisible, setSubMenuVisible] = useState(false)
-  const { active, changeActive, activeChild, changeActiveChild } = useMenuStore()
+  const { active, changeActive, activeChild, changeActiveChild, setActiveBasedOnURL } = useMenuStore()
 
   const toggleShowMenu = () => {
     setMenuVisible(!menuVisible)
@@ -18,34 +18,6 @@ function Menu () {
   const handleSetActive = (newActive, newActiveChild) => {
     changeActive(newActive)
     changeActiveChild(newActiveChild)
-  }
-
-  const setActiveBasedOnURL = () => {
-    const currentURL = window.location.pathname
-
-    const menuItems = {
-      '/servicios/boletos-aereos': { parent: 2, child: 1 },
-      '/servicios/paquetes-turisticos': { parent: 2, child: 2 },
-      '/servicios/seguros-de-viaje': { parent: 2, child: 3 },
-      '/servicios/asesoria-migratoria': { parent: 2, child: 4 },
-      '/servicios/tramites-migratorios': { parent: 2, child: 5 },
-      '/servicios/abogado-y-notario': { parent: 2, child: 6 },
-      '/servicios/aplicaciones-y-sitios-web': { parent: 2, child: 7 },
-      '/servicios/mantenimiento-web': { parent: 2, child: 8 },
-      '/servicios/posicionamiento-web': { parent: 2, child: 9 },
-      '/sobre-nosotros': { parent: 3, child: 0 },
-      '/contacto': { parent: 4, child: 0 },
-      '/': { parent: 1, child: 0 }
-    }
-
-    if (currentURL.startsWith('/tour/')) {
-      menuItems[currentURL] = { parent: 2, child: 2 }
-    }
-
-    const activeMenuItem = menuItems[currentURL] || { parent: 1, child: 0 }
-
-    changeActive(activeMenuItem.parent)
-    changeActiveChild(activeMenuItem.child)
   }
 
   useEffect(() => {
--- src/store/menuStore.js
+++ src/store/menuStore.js
@@ -8,5 +8,32 @@ export const useMenuStore = create((set) => ({
   },
   changeActiveChild: (newChildActive) => {
     set({ activeChild: newChildActive })
+  },
+  setActiveBasedOnURL: () => {
+    const currentURL = window.location.pathname
+
+    const menuItems = {
+      '/servicios/boletos-aereos': { parent: 2, child: 1 },
+      '/servicios/paquetes-turisticos': { parent: 2, child: 2 },
+      '/servicios/seguros-de-viaje': { parent: 2, child: 3 },
+      '/servicios/asesoria-migratoria': { parent: 2, child: 4 },
+      '/servicios/tramites-migratorios': { parent: 2, child: 5 },
+      '/servicios/abogado-y-notario': { parent: 2, child: 6 },
+      '/servicios/aplicaciones-y-sitios-web': { parent: 2, child: 7 },
+      '/servicios/mantenimiento-web': { parent: 2, child: 8 },
+      '/servicios/posicionamiento-web': { parent: 2, child: 9 },
+      '/sobre-nosotros': { parent: 3, child: 0 },
+      '/contacto': { parent: 4, child: 0 },
+      '/': { parent: 1, child: 0 }
+    }
+
+    if (currentURL.startsWith('/tour/')) {
+      menuItems[currentURL] = { parent: 2, child: 2 }
+    }
+
+    const activeMenuItem = menuItems[currentURL] || { parent: 1, child: 0 }
+
+    set({ active: activeMenuItem.parent })
+    set({ activeChild: activeMenuItem.child })
   }
 }))
